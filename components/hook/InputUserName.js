ffc417b3d7f707afb2e66cd27dbae4735d526d92
sendi <sendimuhamad01@gmail.com>
2023-03-18 00:04:00 +0700

    disabled

--- components/hook/InputUserName.js
+++ components/hook/InputUserName.js
@@ -81,6 +81,7 @@ export default function InputUserName(props) {
               name="userID"
               className="mt-1 px-3 py-2 text-blue-500 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
               placeholder="TIDAK PERLU INPUT"
+              disabled
             />
         </div>
       </div>
